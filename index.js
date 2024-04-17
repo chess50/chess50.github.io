1d6d6945437ce1c7f72e09959597fd47772a79a9
chess50 <167095253+chess50@users.noreply.github.com>
2024-04-17 15:36:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,189 @@ var b_king=0,b_queen=0;
 function isAlpha(character) {
     return /^[a-zA-Z]$/.test(character);
 }
+function isLowerCase(char) {
+    return /^[a-z]$/.test(char);
+}
+function isUpperCase(char) {
+    return /^[A-Z]$/.test(char);
+}
+
+function isWhiteCheck(x,y) {
+   
+        //check for pawn checks
+        if(x-1>=0 && x-1<=7 && y-1>=0 && y-1<=7 && board[x-1][y-1]==='p') {console.log("Pawn check!");return true;}
+        if(x-1>=0 && x-1<=7 && y+1>=0 && y+1<=7 && board[x-1][y+1]==='p') {console.log("Pawn check!");return true;}
+        //check for rook checks
+        for(var i=x+1;i<=7;i++){
+            if(board[i][y]==='r' || board[i][y]==='q') {console.log("Rook/queen check!");return true;}
+            else if(board[i][y]===' ' || board[i][y]==='K') continue;
+            else break;
+        }
+        for(var i=x-1;i>=0;i--){
+            if(board[i][y]==='r' || board[i][y]==='q') {console.log("Rook/queen check!");return true;}
+            else if(board[i][y]===' ' || board[i][y]==='K') continue;
+            else break;
+        }
+        for(var i=y+1;i<=7;i++){
+            if(board[x][i]==='r' || board[x][i]==='q') {console.log("Rook/queen check!");return true;}
+            else if(board[x][i]===' ' || board[x][i]==='K') continue;
+            else break;
+        }
+        for(var i=y-1;i>=0;i--){
+            console.log("checking for rook check at "+x+" "+i)
+            if(board[x][i]==='r' || board[x][i]==='q') {console.log("Rook/queen check! returning true");return true;}
+            else if(board[x][i]===' ' || board[x][i]==='K') continue;
+            else break;
+        }
+        //check for bishop checks
+        // console.log("checking for bishop/queen check at")
+        for(var i=x+1,j=y+1;i<=7 && j<=7;i++,j++){
+            if(board[i][j]==='b' || board[i][j]==='q'){console.log("bishop/queen check!");return true;}
+            else if(board[i][j]===' ' || board[i][j]==='K') continue;
+            else break;
+        }
+        for(var i=x-1,j=y-1;i>=0 && j>=0;i--,j--){
+            if(board[i][j]==='b' || board[i][j]==='q'){console.log("bishop/queen check!");return true;}
+            else if(board[i][j]===' ' || board[i][j]==='K') continue;
+            else break;
+        }
+        for(var i=x+1,j=y-1;i<=7 && j>=0;i++,j--){
+            // console.log("checking for bishop/queen check at "+i+" "+j)
+            if(board[i][j]==='b' || board[i][j]==='q') {console.log("bishop/queen check!");return true;}
+            else if(board[i][j]===' ' || board[i][j]==='K') continue;
+            else break;
+        }
+        for(var i=x-1,j=y+1;i>=0 && j<=7;i--,j++){
+            if(board[i][j]==='b' || board[i][j]==='q') {console.log("bishop/queen check!");return true;}
+            else if(board[i][j]===' ' || board[i][j]==='K') continue;
+            else break;
+        }
+        //check for knight checks
+        // console.log("checking for knight check ")
+        if(x+2<=7 && y+1<=7 && board[x+2][y+1]==='n') {console.log("Knight check!");return true;}
+        if(x+2<=7 && y-1>=0 && board[x+2][y-1]==='n') {console.log("Knight check!");return true;}
+        if(x-2>=0 && y+1<=7 && board[x-2][y+1]==='n') {console.log("Knight check!");return true;}
+        if(x-2>=0 && y-1>=0 && board[x-2][y-1]==='n') {console.log("Knight check!");return true;}
+        if(x+1<=7 && y+2<=7 && board[x+1][y+2]==='n') {console.log("Knight check!");return true;}
+        if(x+1<=7 && y-2>=0 && board[x+1][y-2]==='n') {console.log("Knight check!");return true;}
+        if(x-1>=0 && y+2<=7 && board[x-1][y+2]==='n') {console.log("Knight check!");return true;}
+        if(x-1>=0 && y-2>=0 && board[x-1][y-2]==='n') {console.log("Knight check!");return true;}
+      return false;
+
+}
+function checkmateWhite(){
+    var legal_moves = [];
+    var x,y;
+    for(var i=0;i<board.length;i++){
+        for(var j=0;j<board[i].length;j++){
+            if(board[i][j]==='K') {x=i;y=j;break;}
+            }
+    }
+
+    if(x+1<=7 && y+1<=7 && !isWhiteCheck(x+1,y+1)&& (board[x+1][y+1]===' ' || (isLowerCase(board[x+1][y+1]) && isAlpha(board[x+1][y+1])))) legal_moves.push([x+1,y+1]);
+    if(x+1<=7 && y-1>=0 && !isWhiteCheck(x+1,y-1)&& (board[x+1][y-1]===' ' || (isLowerCase(board[x+1][y-1]) && isAlpha(board[x+1][y-1])))) legal_moves.push([x+1,y-1]);
+    if(x-1>=0 && y+1<=7 && !isWhiteCheck(x-1,y+1)&& (board[x-1][y+1]===' ' || (isLowerCase(board[x-1][y+1]) && isAlpha(board[x-1][y+1])))) legal_moves.push([x-1,y+1]);
+    if(x-1>=0 && y-1>=0 && !isWhiteCheck(x-1,y-1)&& (board[x-1][y-1]===' ' || (isLowerCase(board[x-1][y-1]) && isAlpha(board[x-1][y-1])))) legal_moves.push([x-1,y-1]);
+    if(x+1<=7 && !isWhiteCheck(x+1,y)&& (board[x+1][y]===' ' || (isLowerCase(board[x+1][y]) && isAlpha(board[x+1][y]))))legal_moves.push([x+1,y]);
+    if(x-1>=0 && !isWhiteCheck(x-1,y)&& (board[x-1][y]===' ' || (isLowerCase(board[x-1][y]) && isAlpha(board[x-1][y]))))legal_moves.push([x-1,y]);
+    if(y+1<=7 && !isWhiteCheck(x,y+1)&& (board[x][y+1]===' ' || (isLowerCase(board[x][y+1]) && isAlpha(board[x][y+1]))))legal_moves.push([x,y+1]);
+    if(y-1>=0 && !isWhiteCheck(x,y-1)&& (board[x][y-1]===' ' || (isLowerCase(board[x][y-1]) && isAlpha(board[x][y-1]))))legal_moves.push([x,y-1]);
+    console.log(legal_moves)
+
+
+    
+    return legal_moves.length===0 && isWhiteCheck(x,y) && board[x][y]==='K';
+    
+
+}
+function isBlackCheck(x,y) {
+    //check for pawn checks
+    if(x-1>=0 && x-1<=7 && y-1>=0 && y-1<=7 && board[x-1][y-1]==='P') {console.log("Pawn check!");return true;}
+    if(x-1>=0 && x-1<=7 && y+1>=0 && y+1<=7 && board[x-1][y+1]==='P') {console.log("Pawn check!");return true;}
+    //check for rook checks
+    for(var i=x+1;i<=7;i++){
+        if(board[i][y]==='R' || board[i][y]==='Q') {console.log("Rook/queen check!");return true;}
+        else if(board[i][y]===' ' || board[i][y]==='k') continue;
+        else break;
+    }
+    for(var i=x-1;i>=0;i--){
+        if(board[i][y]==='R' || board[i][y]==='Q') {console.log("Rook/queen check!");return true;}
+        else if(board[i][y]===' ' || board[i][y]==='k') continue;
+        else break;
+    }
+    for(var i=y+1;i<=7;i++){
+        if(board[x][i]==='R' || board[x][i]==='Q') {console.log("Rook/queen check!");return true;}
+        else if(board[x][i]===' ' || board[x][i]==='k') continue;
+        else break;
+    }
+    for(var i=y-1;i>=0;i--){
+        console.log("checking for rook check at "+x+" "+i)
+        if(board[x][i]==='R' || board[x][i]==='Q') {console.log("Rook/queen check! returning true");return true;}
+        else if(board[x][i]===' ' || board[x][i]==='k') continue;
+        else break;
+    }
+    //check for bishop checks
+    // console.log("checking for bishop/queen check at")
+    for(var i=x+1,j=y+1;i<=7 && j<=7;i++,j++){
+        if(board[i][j]==='B' || board[i][j]==='Q'){console.log("bishop/queen check!");return true;}
+        else if(board[i][j]===' ' || board[i][j]==='k') continue;
+        else break;
+    }
+    for(var i=x-1,j=y-1;i>=0 && j>=0;i--,j--){
+        if(board[i][j]==='B' || board[i][j]==='Q'){console.log("bishop/queen check!");return true;}
+        else if(board[i][j]===' ' || board[i][j]==='k') continue;
+        else break;
+    }
+    for(var i=x+1,j=y-1;i<=7 && j>=0;i++,j--){
+        // console.log("checking for bishop/queen check at "+i+" "+j)
+        if(board[i][j]==='B' || board[i][j]==='Q') {console.log("bishop/queen check!");return true;}
+        else if(board[i][j]===' ' || board[i][j]==='k') continue;
+        else break;
+    }
+    for(var i=x-1,j=y+1;i>=0 && j<=7;i--,j++){
+        if(board[i][j]==='B' || board[i][j]==='Q') {console.log("bishop/queen check!");return true;}
+        else if(board[i][j]===' ' || board[i][j]==='k') continue;
+        else break;
+    }
+    //check for knight checks
+    // console.log("checking for knight check ")
+    if(x+2<=7 && y+1<=7 && board[x+2][y+1]==='n') {console.log("Knight check!");return true;}
+    if(x+2<=7 && y-1>=0 && board[x+2][y-1]==='n') {console.log("Knight check!");return true;}
+    if(x-2>=0 && y+1<=7 && board[x-2][y+1]==='n') {console.log("Knight check!");return true;}
+    if(x-2>=0 && y-1>=0 && board[x-2][y-1]==='n') {console.log("Knight check!");return true;}
+    if(x+1<=7 && y+2<=7 && board[x+1][y+2]==='n') {console.log("Knight check!");return true;}
+    if(x+1<=7 && y-2>=0 && board[x+1][y-2]==='n') {console.log("Knight check!");return true;}
+    if(x-1>=0 && y+2<=7 && board[x-1][y+2]==='n') {console.log("Knight check!");return true;}
+    if(x-1>=0 && y-2>=0 && board[x-1][y-2]==='n') {console.log("Knight check!");return true;}
+    console.log("returning false reached end ")
+  return false;
+
+    
+}
+function checkmateBlack(){
+    var legal_moves = [];
+    var x,y;
+    for(var i=0;i<board.length;i++){
+        for(var j=0;j<board[i].length;j++){
+            if(board[i][j]==='k') {x=i;y=j;break;}
+            }
+    }
+    if(x+1<=7 && y+1<=7 && !isBlackCheck(x+1,y+1)&& (board[x+1][y+1]===' ' || (isUpperCase(board[x+1][y+1]) && isAlpha(board[x+1][y+1])))) legal_moves.push([x+1,y+1]);
+    if(x+1<=7 && y-1>=0 && !isBlackCheck(x+1,y-1)&& (board[x+1][y-1]===' ' || (isUpperCase(board[x+1][y-1]) && isAlpha(board[x+1][y-1])))) legal_moves.push([x+1,y-1]);
+    if(x-1>=0 && y+1<=7 && !isBlackCheck(x-1,y+1)&& (board[x-1][y+1]===' ' || (isUpperCase(board[x-1][y+1]) && isAlpha(board[x-1][y+1])))) legal_moves.push([x-1,y+1]);
+    if(x-1>=0 && y-1>=0 && !isBlackCheck(x-1,y-1)&& (board[x-1][y-1]===' ' || (isUpperCase(board[x-1][y-1]) && isAlpha(board[x-1][y-1])))) legal_moves.push([x-1,y-1]);
+    if(x+1<=7 && !isBlackCheck(x+1,y)&& (board[x+1][y]===' ' || (isUpperCase(board[x+1][y]) && isAlpha(board[x+1][y]))))legal_moves.push([x+1,y]);
+    if(x-1>=0 && !isBlackCheck(x-1,y)&& (board[x-1][y]===' ' || (isUpperCase(board[x-1][y]) && isAlpha(board[x-1][y]))))legal_moves.push([x-1,y]);
+    if(y+1<=7 && !isBlackCheck(x,y+1)&& (board[x][y+1]===' ' || (isUpperCase(board[x][y+1]) && isAlpha(board[x][y+1]))))legal_moves.push([x,y+1]);
+    if(y-1>=0 && !isBlackCheck(x,y-1)&& (board[x][y-1]===' ' || (isUpperCase(board[x][y-1]) && isAlpha(board[x][y-1]))))legal_moves.push([x,y-1]);
+    console.log(legal_moves)
+
+
+    
+    return legal_moves.length===0 && isBlackCheck(x,y) && board[x][y]==='k';
+
+}
+
 
 function FEN_PARSER(fen) {
     var i = 0, j = 0;
@@ -255,7 +438,6 @@ else{
     FEN_PARSER(fen_parse); 
     if(valid_fen(fen_parse) &&deviceType=='Desktop'){
         applyAnimation();
-      
     var output = '';
     output += '<pre style="font-size: 16px; background-color: black; color: white;">';
     output += '<span style="color:#00FF00;">Parsed Result: </span>\n';
@@ -275,9 +457,27 @@ else{
     output += '<span style="color: #00FF00;">  a   b   c   d   e   f   g   h</span>\n';
     output += '</pre>';
     document.getElementById('chessboard').innerHTML = output;
-    
+    var wx,wy,bx,by
+    for(var i=0;i<board.length;i++){
+        for(var j=0;j<board[i].length;j++){
+            if(board[i][j]==='K') {wx=i;wy=j;}
+            if(board[i][j]==='k') {bx=i;by=j;}
+            }
+        }
     statistics();
     evaluate();
+    // console.log(isBlackCheck(bx,by))
+    if(checkmateWhite()){
+        alert("Checkmate , Black wins!");
+    }
+    else if(checkmateBlack()){
+        alert("Checkmate , White wins!");
+    }
+    else{
+        alert("White in check? :"+isWhiteCheck(wx,wy))
+        alert("Black in check? :"+isBlackCheck(bx,by))
+    
+    }
     
 
 }
